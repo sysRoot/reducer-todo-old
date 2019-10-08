4af9442bd6f2439e4d62f1826c4ab12452ff086e
Jason Cook <the.root.user@gmail.com>
2019-10-08 17:41:48 -0400

    Push now.json

--- todolist/src/state/TodoState.js
+++ todolist/src/state/TodoState.js
@@ -1,10 +1,9 @@
 export const todoState = localStorage.getItem("todoData")
     ? JSON.parse(localStorage.getItem("todoData"))
     : [
-          {
-              description:
-                  "To get started, add an item.",
-              completed: true,
-              id: 1
-          }
-      ];
+        {
+            description: "To get started, add an item.",
+            completed: true,
+            id: 1
+        }
+    ];
